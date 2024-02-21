083cc1ceb06e7ee493ac70a783fc373b375fc963
S M Arifur Rahman <smarifurrahman82@gmail.com>
2024-02-21 12:05:47 +0600

    client comonents used in nextjs using use client

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 
 const HomePage = () => {
+  const [counter, setCounter] = useState(0);
+  // console.log("Hello World!");
   return (
     <div>
       <h1>Hello NextJs</h1>
+      <h2>Counter: {counter}</h2>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Decrease</button>
     </div>
   );
 };
--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,6 +1,6 @@
 const DynamicPage = ({ params, searchParams }) => {
     // console.log(params);
-    // console.log(searchParams);
+    console.log(searchParams);
     return (
         <div>
             <h1>This is dynamic page : {params.id}</h1>
